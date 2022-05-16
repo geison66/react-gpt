d41c2bee481ba2601db9111838a4eaf7e8f1f9ad
Greg Thorson <Engywook2005@gmail.com>
2022-05-16 16:14:49 -0400

    Gregt/negative viewable threshold (#28)
    * Can work with negative viewability threshold.
    
    * Offset in log remark is now VT for clarity.
    
    * Checking for isNaN
    
    * removed logging
    
    Co-authored-by: Engywook2005 <greg.thorson@apartmenttherapy.com>

--- src/Bling.js
+++ src/Bling.js
@@ -389,7 +389,7 @@ class Bling extends Component {
     }
 
     get viewableThreshold() {
-        return this.props.viewableThreshold >= 0
+        return !isNaN(this.props.viewableThreshold)
             ? this.props.viewableThreshold
             : Bling._config.viewableThreshold;
     }
